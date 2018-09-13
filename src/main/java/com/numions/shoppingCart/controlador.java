a1879c82a776cc7bd8c5d3de38238d2fd815fcf9
jcjimenez-stratio <jcjimenez@stratio.com>
2018-09-13 12:04:42 +0200

    Subida del catalogo

---
+++ src/main/java/com/numions/shoppingCart/controlador.java
@@ -0,0 +1,15 @@
+package com.numions.shoppingCart;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//PAtron ressts: Como se comunican las aplicaciones,estructura de endpoints
+@RestController //Se devuele un string , un json etc...
+public class controlador {
+
+
+}
